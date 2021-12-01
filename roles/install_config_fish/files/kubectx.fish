fc59be5ef51d770805ad0aba2c400688bf0be9b1
nazufel <ryan@DESKTOP-44J6BH7.localdomain>
2021-12-01 11:23:14 -0500

    added completions

---
+++ roles/install_config_fish/files/kubectx.fish
@@ -0,0 +1,12 @@
+# kubectx
+
+# usage: completion file for fish shell
+
+function __fish_kubectx_arg_number -a number
+    set -l cmd (commandline -opc)
+    test (count $cmd) -eq $number
+end
+
+complete -f -c kubectx
+complete -f -x -c kubectx -n '__fish_kubectx_arg_number 1' -a "(kubectl config get-contexts --output='name')"
+complete -f -x -c kubectx -n '__fish_kubectx_arg_number 1' -a "-" -d "switch to the previous namespace in this context"
